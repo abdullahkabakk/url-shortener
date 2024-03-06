5abc3279d69dce8982e4048259f90df376f426b6
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2024-03-06 23:43:52 +0300

    [feat] Added database connection
    - Moved database connection logic to a separate file db.go
    - Implemented MySQLConnector struct to encapsulate connection parameters
    - Added NewMySQLConnector function for creating a connector with environment variables
    - Defined Connect method to establish a connection to MySQL
    - Introduced ConnectToDB function for connecting to the database
    - Updated .gitignore file
    - Added 3 new libraries for connecting to database (mysql), getting environment variables (godotenv) and mocking database (go-sqlmock)

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module url-shortener
 
 go 1.22.0
+
+require github.com/go-sql-driver/mysql v1.7.1
+
+require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"os"
+	"url-shortener/db"
+)
+
+// NewMySQLConnector creates a new MySQLConnector with the provided parameters.
+func NewMySQLConnector() *db.MySQLConnector {
+	return &db.MySQLConnector{
+		Username: os.Getenv("DB_USERNAME"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		DBName:   os.Getenv("DB_NAME"),
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file:", err)
+		return
+	}
+
+	// Create MySQLConnector with environment variables
+	connector := NewMySQLConnector()
+
+	// Connect to the database
+	database, err := db.ConnectToDB(connector)
+	if err != nil {
+		fmt.Println("Error connecting to database:", err)
+		return
+	}
+
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			fmt.Println("Error closing database connection:", err)
+		}
+	}(database)
+
+	fmt.Println("Connected to database")
 }
