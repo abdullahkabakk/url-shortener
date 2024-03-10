f64819666a4a964408fc9217381ea62502e87d8e
Abdullah Kabak <05200000101@ogrenci.ege.edu.tr>
2024-03-10 19:30:24 +0300

    [fix] Update database_test

--- internal/infrastructure/database/database_test.go
+++ internal/infrastructure/database/database_test.go
@@ -1,9 +1,11 @@
 package database
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 )
@@ -13,53 +15,56 @@ func TestMySQLConnection(t *testing.T) {
 	if err != nil {
 		fmt.Println("Error loading .env file")
 	}
-
+	//
 	t.Run("Connect to MySQL Database", func(t *testing.T) {
-		username := os.Getenv("DB_USERNAME")
-		fmt.Println(username)
-		//connector := &DBConnector{
-		//	Username: os.Getenv("DB_USERNAME"),
-		//	Password: os.Getenv("DB_PASSWORD"),
-		//	Host:     os.Getenv("DB_HOST"),
-		//	Port:     os.Getenv("DB_PORT"),
-		//	DBName:   os.Getenv("DB_NAME"),
-		//}
-		//
-		//db, err := ConnectToDB(connector, "mysql")
-		//assert.NoError(t, err)
-		//assert.NotNil(t, db)
-		//defer db.Close()
+		connector := &DBConnector{
+			Username: os.Getenv("DB_USERNAME"),
+			Password: os.Getenv("DB_PASSWORD"),
+			Host:     os.Getenv("DB_HOST"),
+			Port:     os.Getenv("DB_PORT"),
+			DBName:   os.Getenv("DB_NAME"),
+		}
+		
+		db, err := ConnectToDB(connector, "mysql")
+		assert.NoError(t, err)
+		assert.NotNil(t, db)
+		defer func(db *sql.DB) {
+			err := db.Close()
+			if err != nil {
+				t.Errorf("Error: %s", err)
+			}
+		}(db)
 	})
 
-	//t.Run("Failed to Connect to MySQL Database", func(t *testing.T) {
-	//	connector := &DBConnector{
-	//		Username: "root",
-	//		Password: "password",
-	//		Host:     "localhost",
-	//		Port:     "3306",
-	//		DBName:   "test",
-	//	}
-	//
-	//	_, err := ConnectToDB(connector, "mysql")
-	//	if err == nil {
-	//		t.Errorf("Expected error, got nil")
-	//	}
-	//})
+	t.Run("Failed to Connect to MySQL Database", func(t *testing.T) {
+		connector := &DBConnector{
+			Username: "root",
+			Password: "password",
+			Host:     "localhost",
+			Port:     "3306",
+			DBName:   "test",
+		}
 
-	//t.Run("Failed to connect for non-existing database", func(t *testing.T) {
-	//	connector := &DBConnector{
-	//		Username: os.Getenv("DB_USERNAME"),
-	//		Password: os.Getenv("DB_PASSWORD"),
-	//		Host:     os.Getenv("DB_HOST"),
-	//		Port:     os.Getenv("DB_PORT"),
-	//		DBName:   "non-existing",
-	//	}
-	//
-	//	_, err := ConnectToDB(connector, "not-existing")
-	//
-	//	assert.Error(t, err)
-	//
-	//})
+		_, err := ConnectToDB(connector, "mysql")
+		if err == nil {
+			t.Errorf("Expected error, got nil")
+		}
+	})
+
+	t.Run("Failed to connect for non-existing database", func(t *testing.T) {
+		connector := &DBConnector{
+			Username: os.Getenv("DB_USERNAME"),
+			Password: os.Getenv("DB_PASSWORD"),
+			Host:     os.Getenv("DB_HOST"),
+			Port:     os.Getenv("DB_PORT"),
+			DBName:   "non-existing",
+		}
+
+		_, err := ConnectToDB(connector, "not-existing")
+
+		assert.Error(t, err)
+
+	})
 
 }
 
